1592aafecd3b8cee67a892efe63bb152553fdc77
mriganksutariya08-netizen <mriganksutariya08@gmail.com>
2025-09-08 20:43:57 +0530

    the game

--- mouse_position.py
+++ mouse_position.py
@@ -1,12 +1,15 @@
 import pyautogui
 import time
 
-time.sleep(5)
+#delay start
+timer = 5
+for i in range(timer,0,-1):
+    print(f'{i}...')
+    time.sleep(1)
+
 
 x,y = pyautogui.position()
-
 pixel_color = pyautogui.screenshot().getpixel((x, y))
-
 
 print(f"x coord is: {x}\ny coord is: {y}")
 print(f"Pixel color at ({x}, {y}) is: {pixel_color}")
